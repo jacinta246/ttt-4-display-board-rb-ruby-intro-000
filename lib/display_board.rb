6b1c384a2083c76ded45f521f496fb40d6c64cc2
Jacinta Calixte <jacinta246@yahoo.com>
2020-03-05 23:44:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,9 @@
 def display_board (board)
-  expect(rows[0]).to eq("   |   |   ")
-  expect(rows[1]).to eq("-----------")
-  expect(rows[2]).to eq("   |   |   ")
-  expect(rows[3]).to eq("-----------")
-  expect(rows[4]).to eq("   |   |   ")
+  puts ["   |   |   "]
+  puts ["-----------"]
+  puts ["   |   |   "]
+  puts ["-----------"]
+  puts ["   |   |   "]
 end
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
