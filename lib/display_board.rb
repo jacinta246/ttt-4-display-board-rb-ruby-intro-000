69cbf9f3a1901301eefe3830ab12b6b7884dd73c
Jacinta Calixte <jacinta246@yahoo.com>
2020-03-06 00:04:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,8 @@
 def display_board (board)
-  puts [0]{"   |   |   "}
-  puts [1]{"-----------"}
-  puts [2]{"   |   |   "}
-  puts [3]{"-----------"}
-end
+  puts ["#{board}|#{board}|#{board}"]
+  puts ["-----------"]
+  puts ["#{board}|#{board}|#{board}"]
+  puts ["-----------"]
+  puts ["#{board}|#{board}|#{board}"]
+  end
+
